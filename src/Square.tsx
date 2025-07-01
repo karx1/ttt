9d9f6d30c3e9c3f9bb709605ae1e6f21a263cf7b
Yash Karandikar <nerdstep710@gmail.com>
2025-07-01 18:41:35 -0500

    Destructure Square props

--- src/Square.tsx
+++ src/Square.tsx
@@ -5,7 +5,7 @@ export enum SquareState {
     None = "",
     X = Turn.X,
     O = Turn.O,
-};
+}
 
 function squareStateFromTurn(t: Turn): SquareState {
     if (t == Turn.X) {
@@ -16,21 +16,23 @@ function squareStateFromTurn(t: Turn): SquareState {
 }
 
 interface SquareProps {
-    currentTurn: Turn,
-    swapTurn: () => void
+    currentTurn: Turn;
+    swapTurn: () => void;
 }
 
-export function Square(props: SquareProps) {
+export function Square({ currentTurn, swapTurn }: SquareProps) {
     let [state, setState] = useState(SquareState.None);
 
     return (
-        // <div className="square" onClick={() => setState(squareStateFromTurn(props.currentTurn))}>
-        <div className="square" onClick={() => {
-            if (state == SquareState.None) {
-                setState(squareStateFromTurn(props.currentTurn));
-                props.swapTurn();
-            }
-        }}>
+        <div
+            className="square"
+            onClick={() => {
+                if (state == SquareState.None) {
+                    setState(squareStateFromTurn(currentTurn));
+                    swapTurn();
+                }
+            }}
+        >
             {state}
         </div>
     );
